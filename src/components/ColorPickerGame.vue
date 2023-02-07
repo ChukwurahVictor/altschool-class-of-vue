02c01a6f25f64b88eb1c40baf0f68634aba5ad2d
Victor <chukwurahvictor7@gmail.com>
2023-02-07 16:49:12 +0100

    refactored using composables

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Color Picker Game</h1>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script>
+import useColorPicker from "../composables/useColorPicker";
+
+export default {
+  setup() {
+    const { colors, message, matchColor } = useColorPicker();
+
+    return { colors, message, matchColor };
+  },
+};
+</script>
+
+<style></style>
